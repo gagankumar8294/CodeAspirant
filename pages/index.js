c0e6d2bc126dfae6cc4a3000620ff5ad602ea75c
Gagan-Sky <gagankumar8294@gmail.com>
2023-06-19 08:52:01 +0530

    basic File & content setup is done

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+// import Navbar from '../comps/Navbar'
+// import Footer from "../comps/Footer"
+import styles from '../styles/Home.module.css'
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CodeAspirant | Home</title>
+        <meta name='keywords' contents="codeaspirant" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>started server on 0.0.0.0:3000 url httplocalhos3000</p>
+        <p className={styles.text}>event  compiled client and server successfully in 3.4s 164 modules</p>
+        <p className={styles.text}>wait compiling client and server event compiled client and server successf</p>
+        <Link className={styles.btn} href="/blog">CodeAspirant just get started</Link>
+      </div>
+    </>
+  )
+}
