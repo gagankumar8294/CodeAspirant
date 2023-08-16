10593ae904d22b77a3ccc38189deea597a93ec3a
Gagan-Sky <YOUR_EMAIL>
2023-08-16 11:35:14 +0530

    added svgs

--- pages/blog/grave.js
+++ pages/blog/grave.js
@@ -24,6 +24,21 @@ const Grave = () => {
       <div style={{textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>
         <p>social icons here</p>
       </div>
+      <section>
+        <div className={styles.social}>
+          <ul>
+            <li>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+              <path d="M29 0h-26c-1.65 0-3 1.35-3 3v26c0 1.65 1.35 3 3 3h13v-14h-4v-4h4v-2c0-3.306 2.694-6 6-6h4v4h-4c-1.1 0-2 0.9-2 2v2h6l-1 4h-5v14h9c1.65 0 3-1.35 3-3v-26c0-1.65-1.35-3-3-3z"></path>
+            </svg>
+            </li>
+            <li>twitter</li>
+            <li>linkedin</li>
+            <li>what's app</li>
+            <li>email</li>
+          </ul>
+        </div>
+      </section>
 
 
       <div className='interact_section'>
