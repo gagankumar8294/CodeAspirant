4e1e1b70702ef4c9c163e9d8723320bac6b0a368
Gagan-Sky <YOUR_EMAIL>
2023-08-16 08:52:03 +0530

    commetns check

--- pages/blog/grave.js
+++ pages/blog/grave.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from "../../styles/Blog.module.css";
 import FacebookComments from '../../comps/comments/FacebookComments';
 import { useState, useEffect } from 'react';
 
-const FirstPost = () => {
+const Grave = () => {
   const [isClient, setIsClient] = useState(false);
   
 
@@ -16,8 +17,12 @@ const FirstPost = () => {
   return (
     <div style={{paddingTop:"150px"}}>
       <div>
-        <h1>My First Blog Post</h1>
-        <p>The facebook comments enabled for legal users to comment</p>
+        <p className={styles.publish}>Published on December 5, 2022 In Endless Origins</p>
+        <h1 className={styles.main_heading}>Another Product Joins the Google Graveyard</h1>
+        <p className={styles.paragraph}>More apps have joined the Google Graveyard, what’s the reason?</p>
+      </div>
+      <div style={{textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>
+        <p>social icons here</p>
       </div>
 
 
@@ -26,7 +31,7 @@ const FirstPost = () => {
             <h4>Left Secion</h4>
         </div>
         <div className='just_type_it'>
-          <FacebookComments href="https://codeaspirant.vercel.app/blog/first-post" />
+          <FacebookComments href="https://codeaspirant.vercel.app/blog/grave" />
         </div>
         <div className='right_ad_posts'>
             <h4>right Secion</h4>
@@ -39,4 +44,4 @@ const FirstPost = () => {
   );
 };
 
-export default FirstPost;
+export default Grave;
