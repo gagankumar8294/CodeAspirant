fb77249f60ffaae996e29ae1e5af354c2eaed9aa
Gagan-Sky <YOUR_EMAIL>
2023-09-10 17:09:38 +0530

    modified 'index.js', added 'AutoTyping.js', added 'c.gif', updated styles for layou of landing page logo and auto text

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from "next/link"
 import Subscribe from '../comps/subscribe/Subscribe'
+import AutoTypingText from '../comps/AutoTypingText';
+import Image from 'next/image';
+
+
+const textList = ['Learn Web Development', 'Learn Essential Tech Tricks', 'We Provide Website Services',]
+
 
 export default function Home() {
   return (
@@ -12,8 +18,20 @@ export default function Home() {
         <title>CodeAspirant | Home</title>
         <meta name='keywords' contents="codeaspirant" />
       </Head>
-      <div style={{paddingTop:"150px"}}>
-        <h1>Welcome to code Aspirant</h1>
+      <div style={{paddingTop:"150px"}} className={styles.container}>  
+        <div className={styles.leftDiv}>
+          <p className={styles.dynamic_text} ><span  style={{color: "#f2f2f2", fontWeight: "bold",}}></span>Welcome to Code Aspirant</p>
+          <p className={styles.dynamic_text2} >
+            <span className={styles.text_bar_pulse}style={{ color: "#f2f2f2", fontWeight: "bold" }}>|</span> <AutoTypingText textList={textList} typingDelay={100} erasingDelay={30} pauseDelay={1500} />
+          </p>
+          <div className={styles.main_link}>
+            <Link className={styles.button_1} href="/blog">Explore Blog</Link>
+            <Link className={styles.button_2} href="/videos">Get Started</Link>
+          </div>
+        </div>
+        <div className={styles.rightDiv} style={{width:"50%"}}>
+          <Image src="/../public/homepage/c.gif" width={300} height={400} alt="CodeAspirant" />
+        </div>  
       </div>
       <div>
         <Subscribe />
