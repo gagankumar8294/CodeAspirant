e04fbf2d6f78efea0d55c6a0444fbaddb6e29fd3
Gagan-Sky <YOUR_EMAIL>
2023-09-23 16:08:01 +0530

    added favicon

--- comps/navbar/Layout.js
+++ comps/navbar/Layout.js
@@ -1,11 +1,17 @@
 import Navbar from "./Navbar"
+import Head from "next/head";
 
 const Layout = ({ children }) => {
     return (
-        <div className="content">
-            <Navbar />
-            { children }
-        </div>
+        <>
+            <Head>
+                <link rel="icon" href="/fav_icon/codeaspirant.png" />
+            </Head>
+            <div className="content">
+                <Navbar />
+                { children }
+            </div>
+        </>
     );
 }
 
