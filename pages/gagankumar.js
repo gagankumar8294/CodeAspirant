d50f5696f0ab2e910924f996d9634409a4a5f659
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-02 09:28:55 +0530

    Update gagankumar.js

--- pages/gagankumar.js
+++ pages/gagankumar.js
@@ -44,7 +44,7 @@ const gagankumar = () => {
             </div>
                                     <div style={{ textAlign: 'center', padding: '20px' }}>
   <h2 className={styles.title} style={{ marginBottom: '20px' }}>Projects</h2>
-                                    <a href="https://bizzwizz.netlify.app/" style={{ display: 'block', marginBottom: '10px', color: '#1a0dab', textDecoration: 'none' }} target="_blank" rel="noopener noreferrer">
+                                    <a href="https://syedmujahid.vercel.app/" style={{ display: 'block', marginBottom: '10px', color: '#1a0dab', textDecoration: 'none' }} target="_blank" rel="noopener noreferrer">
     https://syedmujahid.vercel.app/
   </a>
   <a href="https://bizzwizz.netlify.app/" style={{ display: 'block', marginBottom: '10px', color: '#1a0dab', textDecoration: 'none' }} target="_blank" rel="noopener noreferrer">
