535c885f4cf4c76eebc65b97dd8c4f6751e308a4
Gagan-Sky <gagankumar8294@gmail.com>
2024-08-19 14:22:34 +0530

    removed code just for a while

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
       </div>
 
       {/* Wave SVG */}
-    <div className={serstyles.waveContainer}>
+    {/* <div className={serstyles.waveContainer}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
@@ -59,7 +59,7 @@ export default function Home() {
           d="M0,64L80,85.3C160,107,320,149,480,160C640,171,800,149,960,133.3C1120,117,1280,107,1360,101.3L1440,96V320H1360C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320H0V64Z"
         ></path>
       </svg>
-    </div>
+    </div> */}
 
 
       {/* New Section for SEO-Optimized Content */}
