5831f253c504fbdde554995ad63fe081f92d2855
Gagan-Sky <gagankumar8294@gmail.com>
2024-08-21 20:42:41 +0530

    firebase starter code

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,15 @@
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
+import { db } from "../firebaseinit";
+
+import { collection, addDoc } from "firebase/firestore"; 
+
+// Add a new document with a generated id.
+const docRef = await addDoc(collection(db, "cities"), {
+  name: "Tokyo",
+  country: "Japan"
+});
+console.log("Document written with ID: ", docRef.id);
 
 const blog = () => {
     return (
---
+++ pages/firebaseinit.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";s
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDh42U6u2n9J_kw1NGu4fb7o_6mhNEpbGA",
+  authDomain: "codeaspirant-blog-5cd5b.firebaseapp.com",
+  projectId: "codeaspirant-blog-5cd5b",
+  storageBucket: "codeaspirant-blog-5cd5b.appspot.com",
+  messagingSenderId: "1075942592506",
+  appId: "1:1075942592506:web:da1c41562d82a8e3a81e43",
+  measurementId: "G-V3SL3HWWSJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
