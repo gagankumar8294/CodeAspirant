b10a731305789d5d7d3413b899d47702e50bf0e0
Gagan-Sky <gagankumar8294@gmail.com>
2024-08-22 19:58:40 +0530

    added content to services section

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import Head from 'next/head'
-// import Navbar from '../comps/Navbar'
-// import Footer from "../comps/Footer"
 import styles from '../styles/Home.module.css'
 import Link from "next/link"
-// import Subscribe from '../comps/subscribe/Subscribe'
 import AutoTypingText from '../comps/AutoTypingText';
 import Image from 'next/image';
 import c from ".././public/homepage/c.gif";
+import Services from '../comps/Services/Services.js';
 
-// import PopupForm from '../comps/Animation/PopupForm';
 const textList = ['Learn Web Development', 'Learn Essential Tech Tricks', 'We Provide Website Services',]
-
 
 export default function Home() {
   return (
@@ -20,7 +16,7 @@ export default function Home() {
         <meta name='keywords' contents="codeaspirant" />
       </Head>
       <div style={{paddingTop:"150px"}} className={styles.container}>  
-      {/* <PopupForm /> */}
+
       <div className={styles.leftDiv}>
           <h4 className={styles.dynamic_text} >Welcome to<span className={styles.main_title}>Code Aspirant</span></h4>
           <h2>Code Aspirant</h2>
@@ -47,6 +43,7 @@ export default function Home() {
       {/* <div  className={styles.subscribe} style={{marginTop:"100px"}}>
         <Subscribe />
       </div> */}
+      <Services />
     </>
   )
 }
