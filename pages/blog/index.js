77b6cd8b828adc033526fdf80e6c7bcba9301a4b
Gagan-Sky <gagankumar8294@gmail.com>
2024-09-11 22:51:29 +0530

    blogs comment

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,14 @@
-import Blogs from "@/comps/Blogs/Blogs";
+// import Blogs from "@/comps/Blogs/Blogs";
+import style from '../../styles/Blog.module.css';
 
 const blog = () => {
     return (
         <>
-            <Blogs/>
-        </>
+            {/* <Blogs/> */}
+            <div className={style.containerMargin}>
+               <h2>Blogs are yet to publish</h2>
+            </div>
+                    </>
     );
 }
 
