cb3254c5d3815c9a0e36b1025a2cd59f14095129
Gagan-Sky <gagankumar8294@gmail.com>
2024-09-22 17:35:35 +0530

    added code to fetch blogs from mongodb

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,4 @@
-// import Blogs from "@/comps/Blogs/Blogs";
+import Blogs from "@/comps/Blogs/Blogs";
 import style from '../../styles/Blog.module.css';
 
 const blog = () => {
@@ -13,3 +13,35 @@ const blog = () => {
 }
 
 export default blog;
+
+// pages/blog.js
+// import style from '../../styles/Blog.module.css';
+// import React from 'react';
+
+// export async function getServerSideProps() {
+//   const res = await fetch('http://localhost:3000/api/getBlogs');
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       blogs: data.blogs
+//     }
+//   };
+// }
+
+// const BlogPage = ({ blogs }) => {
+//   return (
+//     <div className={style.container}>
+//       <h1>Blog Posts</h1>
+//       {blogs.map((blog) => (
+//         <div key={blog._id}>
+//           <h2>{blog.title}</h2>
+//           <p>{blog.content}</p>
+//           <small>{new Date(blog.createdAt).toLocaleDateString()}</small>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default BlogPage;
