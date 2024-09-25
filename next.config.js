1cc349b8d490e7c3c8fd8b2de50cd13aa174213a
Gagan-Sky <gagankumar8294@gmail.com>
2024-09-25 22:19:25 +0530

    git solved

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          dns: false,  // Ignore 'dns' module on the client side
+          fs: false,   // Ignore 'fs' (file system) module on the client side
+          net: false,  // Ignore 'net' module on the client side
+          tls: false,  // Ignore 'tls' (transport layer security) module on the client side
+        };
+      }
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
